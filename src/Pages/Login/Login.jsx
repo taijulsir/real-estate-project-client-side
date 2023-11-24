8c40cf12621253fd1b5075d0776002750f9cbe91
taijulsir <princetaijul123@gmail.com>
2023-11-24 21:13:20 +0600

    added login method with email password and googlle

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,35 +4,87 @@ import { LuEye } from "react-icons/lu";
 import { FcGoogle } from "react-icons/fc";
 import { ImGithub } from "react-icons/im";
 import { BsFacebook } from "react-icons/bs";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import useAuth from "../../Hooks/useAuth/useAuth"
 const Login = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+    const {login,googleLogin,githubLogin} = useAuth()
+    const location = useLocation()
+    const navigate = useNavigate()
+    const { register, handleSubmit, formState: { errors },reset } = useForm()
     const onSubmit = (data) => {
-        console.log(data)
+       login(data.email,data.password)
+       .then(result =>{
+        console.log(result.user)
+        reset()
+        toast.success("login succesfull")
+        navigate(location?.state? location.state : '/')
+       })
+       .catch(error=> {
+        console.log(error)
+        toast.error(error.message)
+       })
+    }
+    const handleSocialSignin = (popUp) => {
+        console.log(popUp)
+        popUp()
+        
+        .then(result=>{
+            console.log(result)
+            toast.success("login Succesfull")
+            navigate(location?.state? location.state : '/')
+        })
+        .catch(error=> {
+            console.log(error)
+            toast.error(error.message)
+        })
     }
     return (
         <div>
             <div className="flex justify-center items-center font-[sans-serif] text-[#333] h-full min-h-screen p-4" style={{ backgroundImage: 'url(https://i.ibb.co/Jspy7Nq/register.png)', backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
                 <div className="max-w-md w-full mx-auto">
-                    <form className="bg-opacity-70 bg-white rounded-2xl p-6 shadow-xl">
+                    <form onSubmit={handleSubmit(onSubmit)} className="bg-opacity-70 bg-white rounded-2xl p-6 shadow-xl">
                         <div className="mb-10">
                             <h3 className="text-3xl font-extrabold">Sign in</h3>
                         </div>
+
+                        {/* email  */}
                         <div>
                             <div className="relative flex items-center">
-                                <input name="email" type="text" required
-                                    className="bg-transparent w-full text-sm border-b border-[#333] px-2 py-3 outline-none placeholder:text-[#333]"
-                                    placeholder="Enter email" />
-                                <MdOutlineEmail className="text-2xl " ></MdOutlineEmail>
+                                <MdOutlineEmail className="text-2xl absolute right-3 top-1/2 transform -translate-y-1/2" />
+                                <input
+                                    {...register('email', { required: true })}
+                                    type="email"
+
+                                    className="bg-transparent w-full text-sm border-b border-[#333] px-6 py-3 outline-none placeholder:text-[#333]"
+                                    placeholder="Enter email"
+                                />
                             </div>
+                            {errors.name && <span className="text-red-500 font-medium">This field is required</span>}
                         </div>
+                        {/* password */}
                         <div className="mt-8">
                             <div className="relative flex items-center">
-                                <input name="password" type="password" required
-                                    className="bg-transparent w-full text-sm border-b border-[#333] px-2 py-3 outline-none placeholder:text-[#333]"
-                                    placeholder="Enter password" />
-                                <LuEye className="text-2xl "></LuEye>
+                                <LuEye className="text-2xl absolute right-3 top-1/2 transform -translate-y-1/2" />
+                                <input
+                                    {...register("password", {
+                                        required: true,
+                                        minLength: 6,
+                                        maxLength: 20,
+                                        pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]+$/
+                                    })}
+                                    type="password"
+                                    className="bg-transparent w-full text-sm border-b border-[#333] px-6 py-3 outline-none placeholder:text-[#333]"
+                                    placeholder="Enter password"
+                                />
                             </div>
+                            {errors.password?.type === "required" && <span className="text-red-500 font-medium">This field is required</span>}
+                            {errors.password?.type === "minLength" && <span className="text-red-500 font-medium">Password Must be at least 6 character</span>}
+                            {errors.password?.type === "maxLength" && <span className="text-red-500 font-medium">Password can`t be more than 20 character</span>}
+                            {errors.password?.type === "pattern" && <span className="text-red-500 font-medium">Password have at least one lowercase,uppercase,special character and number</span>}
                         </div>
+
+                        {/* remember and forget password */}
                         <div className="flex items-center justify-between gap-2 mt-6">
                             <div className="flex items-center">
                                 <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 border-gray-300 rounded" />
@@ -41,27 +93,27 @@ const Login = () => {
                                 </label>
                             </div>
                             <div>
-                                <a href="jajvascript:void(0);" className="text-sm font-semibold hover:underline">
+                                <a className="text-sm font-semibold hover:underline">
                                     Forgot Password?
                                 </a>
                             </div>
                         </div>
+
                         <div className="mt-10">
-                            <button type="button"
-                                className="w-full py-2.5 px-4 text-sm font-semibold rounded-full text-white bg-[#333] hover:bg-[#222] focus:outline-none">
+                            <button type="submit" className="w-full py-2.5 px-4 text-sm font-semibold rounded-full text-white bg-[#333] hover:bg-[#222] focus:outline-none">
                                 Sign in
                             </button>
-                            <p className="text-sm text-center mt-6">Don`t have an account <a href="javascript:void(0);"
-                                className="font-semibold hover:underline ml-1 whitespace-nowrap">Register here</a></p>
+                            <p className="text-sm text-center mt-6">Don`t have an account <Link to="/register"
+                                className="font-semibold hover:underline ml-1 whitespace-nowrap">Register here</Link></p>
                         </div>
                         <hr className="my-6 border-gray-500" />
-                        
+
                         {/* social button */}
                         <div className="space-x-8 flex justify-center">
-                            <button type="button" className="border-none outline-none">
+                            <button onClick={()=>handleSocialSignin(googleLogin)} type="button" className="border-none outline-none">
                                 <FcGoogle className="text-4xl"></FcGoogle>
                             </button>
-                            <button type="button" className="border-none outline-none">
+                            <button onClick={()=>handleSocialSignin(githubLogin)} type="button" className="border-none outline-none">
                                 <ImGithub className="text-4xl"></ImGithub>
                             </button>
                             <button type="button" className="border-none outline-none">
@@ -71,6 +123,7 @@ const Login = () => {
                     </form>
                 </div>
             </div>
+            <Toaster></Toaster>
         </div>
     );
 };
