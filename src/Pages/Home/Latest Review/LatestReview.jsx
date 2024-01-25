e455edd634597c0f482e0ce8c58c3529241407e0
taijulsir <princetaijul123@gmail.com>
2024-01-25 13:57:33 +0600

    setup aos in review

--- src/Pages/Home/Latest Review/LatestReview.jsx
+++ src/Pages/Home/Latest Review/LatestReview.jsx
@@ -3,16 +3,21 @@ import useAxiosPublic from "../../../Hooks/useAxiosPublic/useAxiosPublic";
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
-
 // import required modules
-import { Navigation,Pagination,A11y } from 'swiper/modules';
-
-import { CiStar } from "react-icons/ci";
+import { Navigation, Pagination, A11y } from 'swiper/modules';
 import SwiperNavButton from "./SwiperNavButton/SwiperNavButton";
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// import aos css style
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { CiStar } from "react-icons/ci";
+import { useEffect } from "react";
+
 const LatestReview = () => {
     const axiosPublic = useAxiosPublic();
+
+    // fetch review data;;
     const { data: reviews = [] } = useQuery({
         queryKey: ['customerReveiew'],
         queryFn: async () => {
@@ -25,7 +30,19 @@ const LatestReview = () => {
             }
         }
     });
-    console.log(reviews)
+
+
+    // format review date
+    const formatDate = (timestamp) => {
+        const dateObj = new Date(timestamp);
+        const options = { month: 'long', day: 'numeric', year: 'numeric' };
+        return new Intl.DateTimeFormat('en-US', options).format(dateObj);
+    };
+
+    // Aos Animation
+    useEffect(() => {
+        AOS.init();
+    }, []);
     return (
         <div className="my-16 bg-[#F2F6F6] py-16">
             <div className=" max-w-6xl mx-auto flex justify-between items-center flex-col lg:gap-10 lg:flex-row">
@@ -36,8 +53,8 @@ const LatestReview = () => {
                 {/* slider */}
                 <div className="lg:w-3/5 w-4/5">
                     <Swiper
-              
-                        modules={[Navigation,Pagination, A11y]}
+
+                        modules={[Navigation, Pagination, A11y]}
                         spaceBetween={30}
                         slidesPerView="auto"
                         className=" w-full"
@@ -59,7 +76,7 @@ const LatestReview = () => {
                                     {/* review description */}
                                     <div className="bg-[#fff]  font-libreFranklin py-5 px-6 mt-24 rounded-3xl">
                                         <div className=" flex justify-between">
-                                            <h3 className="text-zinc-950 text-lg font-medium"> March 03,2023</h3>
+                                            <h3 className="text-zinc-950 text-lg font-medium">{formatDate(review.reviewTime)}</h3>
                                             <div>
                                                 <img src={review.reviewerImage} alt="" className="h-[100px] w-[100px] rounded-xl bg-sky-200 -mt-[80px] object-cover" />
                                             </div>
@@ -71,7 +88,7 @@ const LatestReview = () => {
                                             <CiStar className="text-amber-400 text-2xl"></CiStar>
                                             <CiStar className="text-amber-400 text-2xl"></CiStar>
                                         </div>
-                                        <p className=" mx-auto my-5 text-base text-[#858484]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur praesentium expedita beatae excepturi quibusdam itaque veritatis molestiae cumque eaque! At totam exercitationem voluptatum ipsa laudantium nulla esse quas. Ullam magnam ea voluptatum laboriosam esse. Explicabo assumenda, quaerat a magni quam quia doloribus quidem nesciunt distinctio, facere qui enim voluptatum rerum.</p>
+                                        <p className=" mx-auto my-5 text-base text-[#858484]">{review.reviewDescription}</p>
                                         <h3 className="text-2xl font-bold text-[#282828]">{review.reviewerName}</h3>
                                         <h4 className="my-2">Texas,Usa</h4>
 
